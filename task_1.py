021ba7bebee89504ecd9450198e0d1a0d33b5232
elizabeth-af <liajkin@yandex.ru>
2026-03-18 19:20:26 +0300

    исправление первого комментария (task_1)

--- task_1.py
+++ task_1.py
@@ -17,11 +17,8 @@ class ExtendedCase(Case):
         self.precondition = precondition
         self.environment = environment
     def print_test_case_info(self):
-        print(f"ID тест-кейса:  {self.test_case_id}"
-              f"\nНазвание: {self.name}"
-              f"\nОписание шага: {self.step_description}"
-              f"\nОжидаемый результат: {self.expected_result}"
-              f"\nПредусловие: {self.precondition}"
+        Case.print_test_case_info(self)
+        print(f"\nПредусловие: {self.precondition}"
               f"\nОкружение: {self.environment}")
 
 case = ExtendedCase(    '1', 'Наличие кнопки Принять', '1. Открыть вкладку приёма документов 2. Проверить наличие кнопки ', 'Кнопка доступна', 'Открыть сервис', 'Яндекс Браузер')
